4df58e4ca58edd9400cb92a01f801d7a4176845b
salah <salahuznr@gmail.com>
2024-02-14 19:53:27 +0530

    controller

--- server/Controllers/ChatController.js
+++ server/Controllers/ChatController.js
@@ -4,4 +4,24 @@ export const createChatModel  = async(req,res) => {
     const newChat  = new ChatModel({
         members :[req.body.senderId,req.body.receiverID] 
     })
+
+    try {
+        const result = await newChat.save();
+        res.status(200).json(result)
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+
+
+export const userChats = async (req,res) =>{
+    try {
+        const chat = await ChatModel.find({
+            members:{$in:[req.params.userId]}
+        }) 
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
